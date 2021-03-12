c2577241651313bf9684b925776fb6310ca9f14c
Sangy K <sansk@users.noreply.github.com>
2021-03-12 20:32:13 +0530

    Updated dev challenge link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="text-center relative right-0 bottom-0 left-0">
-            <p>Created by <a href="https://github.com/sansk" className="font-bold">Sangy K</a> | <a href="https://devchallenges.io">Dev Challenges</a></p>
+            <p>Created by <a href="https://github.com/sansk" className="font-bold">Sangy K</a> | <a href="https://devchallenges.io/solutions/je7ntgloNcSbvvNgcm6p">Dev Challenges</a></p>
         </div>
     );
 };
